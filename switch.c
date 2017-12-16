ad93748765ac4be17e077076f2611c03f09296e1
Antonio <antoniobocanegragalindez@gmail.com>
2017-12-15 22:55:37 -0500

    Estructura selectiva Switch

---
+++ switch.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void main(){
+
+int x,a,b,c,d;
+
+
+scanf("%d", &x);
+
+switch(x){
+	case 1:
+	printf("Se cumplio el caso 1\n");
+	break;
+	
+	case 2:
+	printf("Se cumplio el caso 2\n");
+	break;
+
+	default:
+	printf("No se cumplio ninguno de los casos\n");
+	break;
+ 
+}
+
+
+
+}
+
