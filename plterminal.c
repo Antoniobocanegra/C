c5ab9ac024c13c8943731be6c915b07f1a90caaa
Antonio <antoniobocanegragalindez@gmail.com>
2017-12-16 15:56:58 -0500

    Estructuras Selectivas Anidadas

---
+++ plterminal.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{	
+	char userName[25];
+
+	printf("Bienvenidos a Platzi Terminal\n");
+	printf("Ingresa tu nombre de usuario:\n");
+	scanf("%s", userName);
+
+	printf("Bienvenido %s, vamos a acceder a tu seccion\n", userName);
+
+}
+
